7a07e375de34afec9591f2e8d268c0b0df875b3f
Rafael <66213957+rafaelcoding@users.noreply.github.com>
2020-08-27 11:29:15 -0300

    Update and rename timer to timer.py

--- timer.py
+++ timer.py
@@ -1,6 +1,9 @@
 from time import sleep
+
+
 seconds = int(input('Type how many seconds it is to wait:'))
 print("I'll start in")
+
 sleep(1)
 print('3')
 sleep(1)
@@ -9,6 +12,7 @@ sleep(1)
 print('1')
 sleep(1)
 print('Waiting...')
+
 for cont in range(seconds, -1, -1):
     print(cont)
     sleep(1)
